ceb9a2c4fee4b839d2ac4e56f0c5e6ab16e7cc39
Talia Aziz Khan <talyaaziz@gmail.com>
2021-12-07 04:19:30 +0000

    form added

--- express_server.js
+++ express_server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const app = express();
 const PORT = 8080; // default port 8080
+
 
 app.set("view engine", "ejs");
 
@@ -10,6 +15,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 // Get Requests
 
 
@@ -17,13 +23,20 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -36,8 +49,17 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+function generateRandomString() {
+
+}
