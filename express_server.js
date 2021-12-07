1e5ebf29e1091f4273f883f203e328144f88af36
Talia Aziz Khan <talyaaziz@gmail.com>
2021-12-07 02:55:41 +0000

    updated

--- express_server.js
+++ express_server.js
@@ -4,10 +4,14 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// Get Requests
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -19,6 +23,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
